3ca2795d775a186d056a120c8c3d9c99936db9f3
Hoyeong Choi <1102.ho0@gmail.com>
2024-09-11 21:09:29 +0900

    Revised diet-cal-modal_js.js : Delete JQuery Only JS

--- src/main/resources/static/js/diet-cal-modal_js.js
+++ src/main/resources/static/js/diet-cal-modal_js.js
@@ -1,94 +1,94 @@
-//식단 달력 모달 자바스크립트 파일
+// 식단 모달 관련 자바스크립트
 const dietModal = document.getElementById("diet-modal");
 
 function modalOn() {
-    dietModal.style.display = "flex"
+    dietModal.style.display = "flex";
 }
 
 function isModalOn() {
-    return dietModal.style.display === "flex"
+    return dietModal.style.display === "flex";
 }
 
 function modalOff() {
-    dietModal.style.display = "none"
+    dietModal.style.display = "none";
 }
 
 // 모달창의 클로즈(x) 버튼을 누르면 모달창이 꺼지게 하기
-const calCloseBtn = dietModal.querySelector(".close-btn")
+const calCloseBtn = dietModal.querySelector(".close-btn");
 calCloseBtn.addEventListener("click", e => {
     calBackgroundChange();
-    modalOff()
+    modalOff();
+});
 
-})
 // 모달창 바깥 영역을 클릭하면 모달창이 꺼지게 하기
 dietModal.addEventListener("click", e => {
-    const evTarget = e.target
+    const evTarget = e.target;
     if (evTarget.classList.contains("diet-modal-overlay")) {
         calBackgroundChange();
-        modalOff()
-
+        modalOff();
     }
-})
+});
+
 // 모달창이 켜진 상태에서 ESC 버튼을 누르면 모달창이 꺼지게 하기
 window.addEventListener("keyup", e => {
     if (isModalOn() && e.key === "Escape") {
-        modalOff()
+        modalOff();
         calBackgroundChange();
     }
-})
+});
 
-
-//달력 배경화면 바꾸는 함수 (모달창이 꺼질 때마다 실행)
+// 달력 배경화면 바꾸는 함수 (모달창이 꺼질 때마다 실행)
 let index = 0;
 function calBackgroundChange() {
-    let path = "url(/img/cal_food_";
-    let calBackgroundAddress = [0, 1, 2, 3, 4];
-//url(assets/img/calBackgroundAddress[index])
-    if(index >= calBackgroundAddress.length){
+    const path = "url(/img/cal_food_";
+    const calBackgroundAddress = [0, 1, 2, 3, 4];
+
+    if (index >= calBackgroundAddress.length) {
         index = 0;
-    }
-    else if(index === 0) {
+    } else if (index === 0) {
         index++;
     }
-    let bgAddress = path + calBackgroundAddress[index] + '.png)';
-    $('.wrap-header').css('background-image', bgAddress);
+
+    const bgAddress = path + calBackgroundAddress[index] + '.png)';
+
+    // jQuery 없이 배경 이미지 변경
+    document.querySelector('.wrap-header').style.backgroundImage = bgAddress;
+
     index++;
 }
 
-//영양소 부분의 모달
+// 영양소 모달 관련 자바스크립트
 let nutrientModal = document.getElementById("nutrient-modal");
+
 function modalNutrientOn() {
-    nutrientModal.style.display = "flex"
+    nutrientModal.style.display = "flex";
 }
 
 function isModalNutrientOn() {
-    return nutrientModal.style.display === "flex"
+    return nutrientModal.style.display === "flex";
 }
 
 function modalNutrientOff() {
-    nutrientModal.style.display = "none"
+    nutrientModal.style.display = "none";
 }
 
-
 // 모달창의 클로즈(x) 버튼을 누르면 모달창이 꺼지게 하기
-const closeNutrientBtn = nutrientModal.querySelector(".close-btn")
+const closeNutrientBtn = nutrientModal.querySelector(".close-btn");
 closeNutrientBtn.addEventListener("click", e => {
-    modalNutrientOff()
+    modalNutrientOff();
+});
 
-})
 // 모달창 바깥 영역을 클릭하면 모달창이 꺼지게 하기
 nutrientModal.addEventListener("click", e => {
-    const evTarget = e.target
+    const evTarget = e.target;
     if (evTarget.classList.contains("modal-overlay")) {
-        modalNutrientOff()
-
+        modalNutrientOff();
     }
-})
+});
+
 // 모달창이 켜진 상태에서 ESC 버튼을 누르면 모달창이 꺼지게 하기
 window.addEventListener("keyup", e => {
     if (isModalNutrientOn() && e.key === "Escape") {
-        modalNutrientOff()
+        modalNutrientOff();
     }
-})
-
-
+});
